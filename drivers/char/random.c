27af286898ac5283c59b872fc41ae2578d264c7e
bhi768 <bhimanand7@gmail.com>
2020-04-17 09:13:07 +0000

    Fix derp

--- drivers/char/random.c
+++ drivers/char/random.c
@@ -2263,12 +2263,16 @@ u64 get_random_u64(void)
 	struct batched_entropy *batch;
 	static void *previous;
 
-	hash = get_cpu_var(get_random_int_hash);
+#if BITS_PER_LONG == 64
+	if (arch_get_random_long((unsigned long *)&ret))
+		return ret;
+#else
+	if (arch_get_random_long((unsigned long *)&ret) &&
+	    arch_get_random_long((unsigned long *)&ret + 1))
+	    return ret;
+#endif
 
-	hash[0] += current->pid + jiffies + random_get_entropy();
-	md5_transform(hash, random_int_secret);
-	ret = hash[0];
-	put_cpu_var(get_random_int_hash);
+	warn_unseeded_randomness(&previous);
 
 	batch = raw_cpu_ptr(&batched_entropy_u64);
 	spin_lock_irqsave(&batch->batch_lock, flags);
@@ -2292,12 +2296,10 @@ u32 get_random_u32(void)
 	struct batched_entropy *batch;
 	static void *previous;
 
-	hash = get_cpu_var(get_random_int_hash);
+	if (arch_get_random_int(&ret))
+		return ret;
 
-	hash[0] += current->pid + jiffies + random_get_entropy();
-	md5_transform(hash, random_int_secret);
-	ret = *(unsigned long *)hash;
-	put_cpu_var(get_random_int_hash);
+	warn_unseeded_randomness(&previous);
 
 	batch = raw_cpu_ptr(&batched_entropy_u32);
 	spin_lock_irqsave(&batch->batch_lock, flags);
